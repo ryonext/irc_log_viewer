0ead4faa63848ec13f09db70440899565a1658ed
ryonext <ryonext.s@gmail.com>
2016-06-24 15:57:05 +0900

    Save and display user name and time

--- app/models/irc_log.rb
+++ app/models/irc_log.rb
@@ -4,23 +4,30 @@ class IrcLog
 
   def save
     channel = Channel.find_or_create_by(name: channel_name)
+    date_str = /^(\d\d\d\d-\d\d-\d\d)?.+$/.match(log_file.original_filename)[1]
     # Open file
     # each lines
     File.foreach(log_file.tempfile) do |line|
-      build_message_from_log(line, channel)
+      build_message_from_log(line, channel, date_str)
     end
     # fetch each object
     # import record
   end
 
-  def build_message_from_log(log, channel)
+  def build_message_from_log(log, channel, date_str)
     result = /^(\d\d:\d\d)? (.+)?: (.+)?$/.match(log)
     unless result
       return nil
     end
     time = result[1]
-    user = result[2]
+    name = result[2]
     text = result[3]
-    Message.create(text: text, channel: channel)
+    user = User.find_or_create_by(name: name)
+    Message.create(
+      user: user,
+      text: text,
+      channel: channel,
+      published_at: Time.parse("{date_str} #{time}")
+    )
   end
 end
