f09ac10dfd7c213447fec8afc2570ccfe3ab107d
ryonext <ryonext.s@gmail.com>
2016-06-28 18:43:08 +0900

    Fix app path.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,5 +1,5 @@
 # paths
-app_path = "/var/www/irc_logger"
+app_path = "/var/www/irc_log_viewer"
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
