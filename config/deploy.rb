cbb15dbe64968be03cf64c47458e65527d3d186a
ryonext <ryonext.s@gmail.com>
2016-06-28 17:57:36 +0900

    Run db create before migration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,15 @@ set :rbenv_type, :user
 set :rbenv_ruby, "2.3.1"
 
 namespace :deploy do
+  before :migrate, :create_db do
+    on roles(:db), in: :groups, limit: 3, wait: 10 do
+      with rails_env: fetch(:rails_env) do
+        within release_path do
+          execute :rake, "db:create"
+        end
+      end
+    end
+  end
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
